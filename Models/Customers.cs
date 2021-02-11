a8606db26fed986d187442ec9472560be27e3dae
Matthew Cullen <m23.cullen@gmail.com>
2021-02-11 16:48:01 -0500

    add Customers.cs Model and add #nullable enable/disable section to Track.cs

---
+++ Models/Customers.cs
@@ -0,0 +1,21 @@
+namespace Models
+{
+    public class Customers
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        //NULLABLE PROPERTIES
+        #nullable enable
+        public string? Company { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Phone { get; set; }
+        public string? Fax { get; set; }
+        #nullable disable
+    }
+}
--- Models/Track.cs
+++ Models/Track.cs
@@ -4,12 +4,15 @@ namespace Models
     {
         public int TrackId { get; set; }
         public string Name { get; set; }
-        public int? AlbumId { get; set; }
         public int MediaTypeId { get; set; }
-        public int? GenreId { get; set; }
         public string Composer { get; set; }
         public int Milliseconds { get; set; }
-        public int? Bytes { get; set; }
         public int UnitPrice { get; set; }
+        //NULLABLE PROPERTIES
+        #nullable enable
+        public int? AlbumId { get; set; }
+        public int? Bytes { get; set; }
+        public int? GenreId { get; set; }
+        #nullable disable
     }
 }
